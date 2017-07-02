729b41bc4171f62a206bbb1c2d2141d26a68aaf9
Vitalii <ridikon666@gmail.com>
2017-07-02 19:24:24 +0000

    Initial

--- js/send-form.js
+++ js/send-form.js
@@ -1,25 +1,25 @@
-$(function () {
-	$('.main__form-submit').click(sendForm);
-	
+$(function() {
+	$('#main__form-submit').click(sendForm);
+
 });
 
-function sendForm(e){
+function sendForm(e) {
 	e.preventDefault();
 	$.ajax({
-		url: "https://formspree.io/ridikon666@gmail.com", 
+		url: "https://formspree.io/ridikon666@gmail.com",
 		method: "POST",
 		data: {
 			name: $('#client-name').val(),
 			tel: $('#client-phone').val(),
-			tel: $('#client-phone').val(),
+			email: $('#client-email').val(),
 			text: $('#client-text').val(),
 			pass: $('#client-password').val(),
 			city: $('#client-city').val(),
 			gender: $('#gender option').val()
 		},
 		dataType: "json"
-	});
-	.done(function(){
-		$('form').html('<h1>Дякуємо!!!</h1>');
+	})
+	.done(function() {
+		alert('Дякуємо!!!');
 	});
 }

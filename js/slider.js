26e57b5344fa0001c141dba4398a93a6aff66fc4
Vitalii <ridikon666@gmail.com>
2017-07-05 11:03:30 +0000

    Initial

---
+++ js/slider.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('.header__carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    dots: true,
+  	slidesToShow: 3,
+ 	slidesToScroll: 3,
+ 	 responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+});
+	
